b54fb2525b277d92b81c798940f509a768aa3aa5
Kirill Rudenko <cs1@list.ru>
2022-08-25 17:54:04 +0300

    feat: функция 'predict_rub_salary' вычисления средней зарплаты
    - функция 'predict_rub_salary' вычисляет среднюю зарплату одной вакансии
    - сделал вычисление средней зарплаты всех вакансий по каждому языку программирования
    - функция 'get_hh_vacancies' возвращает информацию по вакансиям для каждого языка программирования

--- main.py
+++ main.py
@@ -2,34 +2,48 @@ import requests
 from pprint import pprint
 
 
-def get_hh_vacancies():
-    programming_languages = dict.fromkeys(
+def predict_rub_salary(vacancy: dict) -> float:
+    if (vacancy["salary"]["currency"] != "RUR" or 
+        (vacancy["salary"]["from"] is None and vacancy["salary"]["to"] is None)):
+        return None
+    elif vacancy["salary"]["from"] and vacancy["salary"]["to"]:
+        return (vacancy["salary"]["from"] + vacancy["salary"]["to"]) / 2
+    elif vacancy["salary"]["from"]:
+        return vacancy["salary"]["from"] * 1.2
+    else:
+        return vacancy["salary"]["to"] * 0.8
+
+
+def get_hh_vacancies() -> dict:
+    programming_languages_vacansies = dict.fromkeys(
         ["Python", "C", "Java", "C++", "C#", "Javacsript", "PHP", "Go", "Swift", "Kotlin"]
     )
-
-    for lang in programming_languages:
-        data = {
+    for lang in programming_languages_vacansies:
+        params = {
             "area": 1,  # 1 - Москва
             "text": f"Программист {lang}",
             "period": 30,
             "currency": "RUR",
-            "gross": False,
             "only_with_salary": True,
         }
-        response = requests.get('https://api.hh.ru/vacancies', params=data)
+        response = requests.get('https://api.hh.ru/vacancies', params=params)
         response.raise_for_status()
-        programming_languages[lang] = response.json()["found"]
+        vacancies = response.json()["items"]
 
-    pprint(programming_languages)
-    return response.json()['items']
-
+        salaries = [predict_rub_salary(vacancy) for vacancy in vacancies if predict_rub_salary(vacancy)]
+        average_salary = sum(salaries) / len(salaries)
+        programming_languages_vacansies[lang] = {
+            "vacancies_found": response.json()["found"],
+            "vacancies_processed": len(salaries),
+            "average_salary": int(average_salary)
+        }
+    return programming_languages_vacansies
 
 def main():
     try: 
-        get_hh_vacancies()
+        pprint(get_hh_vacancies())
     except requests.exceptions.HTTPError as error:
         print(error, error.response.text, sep="\n")
     
 if __name__ == '__main__':
     main()
-
